77e12fec0eb197b9503d2f624f12db4d991cd525
crecat <112257840+Ben-Kei-create@users.noreply.github.com>
2024-10-05 23:41:50 +0900

    2024/10/05

--- Script/ballScript.cs
+++ Script/ballScript.cs
@@ -16,14 +16,25 @@ public class ballScript : MonoBehaviour
     void Update()
     {
         transform.position += new Vector3(0f, 0f, -1 * speed * Time.deltaTime);
+                if(transform.position.z < -13.0f)
+        {
+            Debug.Log("GameOver");
+            Time.timeScale = 0;
+        }   
     }
 
     private void OnCollisionEnter(Collision collision) // Removed the 's' here
     {
-        if(collision.gameObject.CompareTag("Paddle"))
+        if (collision.gameObject.CompareTag("Paddle"))
         {
             // ボールを破棄する
             Destroy(gameObject);
+            // 
+            collision.gameObject.transform.localScale -= new Vector3(Random.Range(0.2f, 1.0f),0f,0f);
+            if(collision.gameObject.transform.localScale.x < 1.0f)
+            {
+                collision.gameObject.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            }
         }
     } // Ensure this closing brace is for the class
 } // Ensure this is the closing brace for the ballScript class
